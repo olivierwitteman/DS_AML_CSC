5742b3a2794e28fc22eed8c406e1152397d77ebe
Olivier Witteman <olivier@2001.net>
2019-11-23 21:51:42 +0100

    gezellig zaterdagavond

--- Main.py
+++ Main.py
@@ -1,8 +1,11 @@
 from Models import CSC_lib
+import pandas as pd
 
 CSC = CSC_lib.CSC()
 
 X_train, y_train, X_test = CSC.load_sets()
 
+print(X_train.keys())
+# print(CSC.return_sentence(X_train, 4)[X_train.keys()[1]])
 
-print(X_train)
+print(CSC.bag_of_words(['jop is een een een mooie jongen', 'gideon ook is']))
--- Models/CSC_lib.py
+++ Models/CSC_lib.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
 
 
 class CSC:
@@ -11,4 +12,11 @@ class CSC:
         train_data = pd.read_csv('{!s}/train_data.csv'.format(path))
         return train_data, train_answers, test_data
 
+    def return_sentence(self, df, sentence_id=1):
+        return df[df['id'] == 'sentence_{!s}'.format(sentence_id)]
 
+    def bag_of_words(self, text):
+        vectorizer = CountVectorizer()
+        bow = vectorizer.fit_transform(text)
+        feature_names = vectorizer.get_feature_names()
+        return bow #, feature_names
